b95b3fa65231221e04e7c89e1d5918e98ecee111
haocwang <haocwang@umich.edu>
2019-12-08 00:54:44 -0500

    added setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from distutils.core import setup
 setup(
-  name = 'FairAutoML',         # How you named your package folder (MyLib)
+  name = 'FairAutoML',         # How you named your package folder
   packages = ['FairAutoML'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Fairness-aware AutoML System',   # Give a short description about your library
   author = 'Haochen Wang',                   # Type in your name
   author_email = 'hw1985@nyu.edu',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/user/haocwang',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/haocwang/FairAutoML/archive/v0.1-alpha.tar.gz',    # I explain this later on
   keywords = ['autoML', 'fairness', 'Machine Learning'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[            
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'auto-sklearn',
+          'aif360',
+          'tqdm',
+          'pyrfr',
+          'cvxpy',
+          'numba',
+          'BlackBoxAuditing',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
